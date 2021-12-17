85b3610cf51e5dc481a26d6e18ad51c9710810f9
Mljungho <mathias.ljungholm@gmail.com>
2021-12-17 18:33:33 +0100

    Creates redux folders and basic setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
+import configureStore from "./state/store/configureStore";
 
+const store = configureStore();
+
+window.store = store;
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
-)
+  document.getElementById("root")
+);
